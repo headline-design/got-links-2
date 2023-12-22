4946a31f93d251532f28bfb8359572d41f59079d
HEADLINE Design <aaron@headline.dev>
2023-12-22 15:34:57 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ const conf = {
   actionItems: [
     {
       label: "email",
-      btnContent:  <a href="mailto:contact@headline-inc.com" aria-label="email" class="Header__StyledGlobalAction-sc-14tpcyv-1 hzqpaE bx--header__action-1" type="button"><svg focusable="false" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" fill="white" description="Icon" width="24" height="24" viewBox="0 0 32 32" aria-hidden="true"><path d="M28,6H4A2,2,0,0,0,2,8V24a2,2,0,0,0,2,2H28a2,2,0,0,0,2-2V8A2,2,0,0,0,28,6ZM25.8,8,16,14.78,6.2,8ZM4,24V8.91l11.43,7.91a1,1,0,0,0,1.14,0L28,8.91V24Z"></path></svg></a>
+      btnContent:  <a href="mailto:contact@headline.dev" aria-label="email" class="Header__StyledGlobalAction-sc-14tpcyv-1 hzqpaE bx--header__action-1" type="button"><svg focusable="false" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" fill="white" description="Icon" width="24" height="24" viewBox="0 0 32 32" aria-hidden="true"><path d="M28,6H4A2,2,0,0,0,2,8V24a2,2,0,0,0,2,2H28a2,2,0,0,0,2-2V8A2,2,0,0,0,28,6ZM25.8,8,16,14.78,6.2,8ZM4,24V8.91l11.43,7.91a1,1,0,0,0,1.14,0L28,8.91V24Z"></path></svg></a>
     },
     {
       btnContent: (
